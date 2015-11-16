89f08f7712249ba16fff6e9d0a6c2ed82fc25bd4
wenqing <yuwenqingisu@gmail.com>
2015-11-16 16:19:34 +0000

    aa

--- api/product/getProductInfo.php
+++ api/product/getProductInfo.php
@@ -15,7 +15,7 @@ global $client;
 // print_r($client->products->get_count());
 
 echo "<br> Product: <br>";
-print_r($client->products->get());
+print_r($client->products->get_by_sku('001'));
 
 // echo "<br> Orders: <br>";
 // print_r($client->orders->get());
